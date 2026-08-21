8ec08e714146de2dccad4613bb18a89bac8b3b21
mahamad25 <bodlapatimahamad@gmail.com>
2026-08-21 09:33:11 +0530

    Add Java program to find pair with target sum

---
+++ week 4/session2.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter sorted array elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter target: ");
+        int target = sc.nextInt();
+
+        int left = 0;
+        int right = n - 1;
+
+        boolean found = false;
+
+        while (left < right) {
+
+            int sum = arr[left] + arr[right];
+
+            if (sum == target) {
+                System.out.println(
+                    "Pair found: " + arr[left] + " + " + arr[right]
+                );
+                found = true;
+                break;
+            } 
+            else if (sum < target) {
+                left++;
+            } 
+            else {
+                right--;
+            }
+        }
+
+        if (!found) {
+            System.out.println("No pair found.");
+        }
+
+        sc.close();
+    }
+}
