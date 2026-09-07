144e433d956c6148ca86e841aeb2c36d208081f8
mahamad25 <bodlapatimahamad@gmail.com>
2026-09-07 10:24:00 +0530

    Create session3.1.java

---
+++ week 7/session3.1.java
@@ -0,0 +1,22 @@
+class MyHashSet {
+    private boolean[] set;
+
+    public MyHashSet() {
+        set = new boolean[1000001]; // 1e6 + 1
+    }
+
+    public void add(int key) {
+        set[key] = true;
+    }
+
+    public void remove(int key) {
+        set[key] = false;
+    }
+
+    public boolean contains(int key) {
+        return set[key];
+    }
+}
+output
+  ["MyHashSet", "add", "add", "contains", "contains", "add", "contains", "remove", "contains"]
+[[], [1], [2], [1], [3], [2], [2], [2], [2]]
